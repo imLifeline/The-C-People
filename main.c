0728496e6f2446c8055afd882fef5c67b6fc1327
imLifeline <hhhunterwolff@gmail.com>
2024-01-16 07:03:23 -0800

    A simple function

--- main.c
+++ main.c
@@ -1,3 +1,32 @@
-//
-// Created by imLifeline on 1/16/2024.
-//
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isNumber(const char input[]);
+
+int main() {
+    char input[50];
+
+    printf("Enter a number: ");
+    scanf("%s", input);
+    if(!isNumber(input)) {
+        printf("Invalid input!\n");
+        return 1;
+    }
+    printf("%s is a number\n", input);
+
+}
+
+bool isNumber(const char input[]) {
+    int i = 0;
+    bool result = true;
+
+    while(input[i] != '\0') {
+        if(input[i] < '0' || input[i] > '9') {
+            result = false;
+            break;
+        }
+        i++;
+    }
+
+    return result;
+}
